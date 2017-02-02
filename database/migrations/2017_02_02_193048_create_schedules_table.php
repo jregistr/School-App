d5c9fdf8192de4ed938d8932ca25b955bb21207e
Jeff Registre <jeffreyregist365@gmail.com>
2017-02-02 14:42:27 -0500

    Added migration bug fix and some migrations.
    Added line to AppServiceProvider from laravel docs to fix migration issue.
    Added migration files for schools, schedules, classes. section is
    added but isnt' filled out.

---
+++ database/migrations/2017_02_02_193048_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('student_id');
+            $table->boolean('selected');
+
+            $table->foreign('student_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
