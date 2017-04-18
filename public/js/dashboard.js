29de43bcd027e3d7c18c8c648cd5b6733ae49105
Jeff Registre <jeffreyregist365@gmail.com>
2017-04-17 22:07:43 -0400

    updated main template

---
+++ public/js/dashboard.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jeff on 4/17/17.
+ */
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,15 @@
-@extends('layouts.app')
+@extends('layouts.main')
+
+@section('stylesheets')
+    <link rel="stylesheet" href="css/dashboard.css">
+@endsection
+
+@section('javascripts')
+    <script src="js/dashboard.js"></script>
+@endsection
 
 @section('content')
-<div class="container">
+    {{--<div class="container">--}}
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
@@ -9,9 +17,22 @@
 
                 <div class="panel-body">
                     You are logged in!
+                    <ul>
+                        <li>
+                            <a href="{{ url('/logout') }}"
+                               onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-</div>
+    {{--</div>--}}
 @endsection
