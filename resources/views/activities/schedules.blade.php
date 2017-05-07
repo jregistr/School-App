011fabeeb252406a51bc0c78700f471cff09ec3e
Jeff Registre <jeffreyregist365@gmail.com>
2017-05-07 16:05:38 -0400

    Commit before switching to laravel mix for front end.

--- resources/views/activities/schedules.blade.php
+++ resources/views/activities/schedules.blade.php
@@ -15,27 +15,11 @@
         <div class="">
             <div class="panel panel-default">
                 <div class="panel-heading">Schedules</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                    <ul>
-                        <li>
-                            <a href="{{ url('/logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                <strong>Logout</strong>
-                            </a>
-
-                            <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-
-                        </li>
-                    </ul>
-
-                </div>
             </div>
         </div>
+    </div>
+    <div class="row">
+        <div id="calender"></div>
     </div>
     {{--</div>--}}
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -111,7 +111,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
         crossorigin="anonymous"></script>
-<script src="js/app.js"></script>
+<script src="js/ app.js"></script>
 <script src="js/main-template.js"></script>
 @yield('javascripts')
 </body>
